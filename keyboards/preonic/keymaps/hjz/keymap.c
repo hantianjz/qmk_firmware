f607875b8701cfa5555abf7643dac3dc2fdd2e5b
James Zhao <hantian@google.com>
2020-11-29 21:20:31 -0500

    Personal preonic layout

---
+++ keyboards/preonic/keymaps/hjz/keymap.c
@@ -0,0 +1,101 @@
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    TMUX_Q = SAFE_RANGE,
+    TMUX_A,
+    TMUX_Z,
+    TMUX_W,
+    TMUX_S,
+    TMUX_X,
+    TMUX_D,
+    TMUX_C,
+    TMUX_V,
+    TMUX_N,
+    TMUX_M,
+    TMUX_COMMA,
+    TMUX_OPEN_SQ_B,
+    TMUX_CLOSE_SQ_B,
+    TMUX_CTRL_O,
+    TMUX_P,
+    TMUX_COLON,
+    TMUX_MINUS,
+    TMUX_PLUS,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_ortho_5x12(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT, KC_LCTL, KC_LALT, KC_INS, KC_LGUI, MO(2), KC_SPC, KC_SPC, MO(1), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+                                                              [1] = LAYOUT_ortho_5x12(KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, MO(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, TMUX_COLON, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
+                                                              [2] = LAYOUT_ortho_5x12(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_HOME, KC_PGDN, KC_PGUP, KC_END),
+                                                              [3] = LAYOUT_ortho_5x12(KC_TRNS, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MODE_FORWARD, RGB_TOG, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS),
+                                                              [4] = LAYOUT_ortho_5x12(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_TRNS, TMUX_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, TMUX_CTRL_O, TMUX_P, KC_TRNS, KC_TRNS, TMUX_A, TMUX_S, TMUX_D, KC_F, KC_G, KC_H, TMUX_MINUS, TMUX_PLUS, TMUX_OPEN_SQ_B, TMUX_CLOSE_SQ_B, KC_TRNS, KC_TRNS, TMUX_Z, TMUX_X, TMUX_C, TMUX_V, KC_B, TMUX_N, TMUX_M, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
+
+void tmux_code(keyrecord_t *record, char *tmux_code) {
+    if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a") SS_DELAY(100));
+        send_string(tmux_code);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TMUX_Q:
+            tmux_code(record, "q");
+            break;
+        case TMUX_A:
+            tmux_code(record, "a");
+            break;
+        case TMUX_Z:
+            tmux_code(record, "z");
+            break;
+        case TMUX_W:
+            tmux_code(record, "w");
+            break;
+        case TMUX_S:
+            tmux_code(record, "s");
+            break;
+        case TMUX_X:
+            tmux_code(record, "x");
+            break;
+        case TMUX_D:
+            tmux_code(record, "d");
+            break;
+        case TMUX_C:
+            tmux_code(record, "c");
+            break;
+        case TMUX_V:
+            tmux_code(record, "v");
+            break;
+        case TMUX_N:
+            tmux_code(record, "n");
+            break;
+        case TMUX_M:
+            tmux_code(record, "m");
+            break;
+        case TMUX_COMMA:
+            tmux_code(record, ",");
+            break;
+        case TMUX_OPEN_SQ_B:
+            tmux_code(record, "[");
+            break;
+        case TMUX_CLOSE_SQ_B:
+            tmux_code(record, "]");
+            break;
+        case TMUX_CTRL_O:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("a") SS_DELAY(100) SS_LCTL("o"));
+            }
+            break;
+        case TMUX_P:
+            tmux_code(record, "p");
+            break;
+        case TMUX_COLON:
+            tmux_code(record, ":");
+            break;
+        case TMUX_MINUS:
+            tmux_code(record, "-");
+            break;
+        case TMUX_PLUS:
+            tmux_code(record, "=");
+            break;
+    }
+    return true;
+};
