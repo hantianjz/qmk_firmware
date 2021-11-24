00b6cbd50fd2294ee8a9cd102419e53ce0edac9f
James Zhao <jamzhao@gmail.com>
2021-11-24 14:14:38 -0500

    Remove 2 more keys from ergodox_ez hjz keymap

--- keyboards/ergodox_ez/keymaps/hjz/keymap.c
+++ keyboards/ergodox_ez/keymaps/hjz/keymap.c
@@ -25,6 +25,23 @@ enum custom_keycodes {
     TMUX_PLUS,
 };
 
+// Tap Dance declarations
+enum {
+    TD_LD,
+    TD_RU,
+    TD_PDHM,
+    TD_PUED,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_LD] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_DOWN),
+    [TD_RU] = ACTION_TAP_DANCE_DOUBLE(KC_RGHT, KC_UP),
+    [TD_PDHM] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_HOME),
+    [TD_PUED] = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_END),
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Qwerty Basic layer
@@ -38,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   '    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |  Alt |  INS |  GUI |                                       |_NUMBE| Left | Down |  Up  |Right |
+ *   |      |      |  Alt |  INS |  GUI |                                       | Left | Down |  Up  |Right |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      |      |       |      |        |
@@ -54,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_TRNS,              KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LCTL_T(KC_TAB),  KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_TRNS,              KC_TRNS,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_TRNS,         KC_TRNS,     KC_LALT,       KC_INS,  KC_LGUI,                                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
+  KC_TRNS,         KC_TRNS,     KC_LALT,       KC_INS,  KC_LGUI,                                         TD(TD_LD),   TD(TD_RU),KC_TRNS, KC_TRNS, KC_TRNS,
                                                                  KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS,
                                                                           KC_TRNS,                KC_TRNS,
                                               LT(_FUNC, KC_SPC), KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, LT(_NUMBER, KC_SPC)
@@ -86,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,     KC_TRNS, KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    KC_DEL,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   MO(_SYSTM),  KC_MINS, KC_EQL,     KC_LBRC, KC_RBRC, TMUX_COLON,
   KC_TRNS, TMUX_Z,  TMUX_X,  TMUX_C,  TMUX_V,  KC_TRNS, KC_TRNS,     KC_TRNS, TMUX_N,  TMUX_M,  TMUX_COMMA, KC_TRNS, KC_TRNS, KC_BSLS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS,  KC_HOME,    KC_PGDN, KC_PGUP, KC_END,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     TD(TD_PDHM), TD(TD_PUED), KC_TRNS, KC_TRNS,  KC_TRNS,
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
                                     MO(_TMUX), KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
@@ -114,11 +131,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_FUNC] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL,
-  KC_LCTL, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,       KC_F9,   KC_F10,   KC_DEL,
+  KC_LCTL, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,                       KC_LEFT, KC_DOWN, KC_UP,       KC_RGHT, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     TD(TD_PDHM), TD(TD_PUED), KC_TRNS, KC_TRNS,  KC_TRNS,
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
                                       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, MO(_TMUX)
