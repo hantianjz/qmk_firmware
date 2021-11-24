4d06d19a2d69eaa29357cc1754a5cedc16244d07
James Zhao <jamzhao@gmail.com>
2021-11-24 14:40:13 -0500

    remove 2 more keys on ergodox_ez 42 keys

--- keyboards/ergodox_ez/keymaps/hjz/keymap.c
+++ keyboards/ergodox_ez/keymaps/hjz/keymap.c
@@ -31,15 +31,16 @@ enum {
     TD_RU,
     TD_PDHM,
     TD_PUED,
+    TD_BKDEL,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_LD] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_DOWN),
-    [TD_RU] = ACTION_TAP_DANCE_DOUBLE(KC_RGHT, KC_UP),
-    [TD_PDHM] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_HOME),
-    [TD_PUED] = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_END),
+    [TD_LD]    = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_DOWN),  // Left,  Down
+    [TD_RU]    = ACTION_TAP_DANCE_DOUBLE(KC_RGHT, KC_UP),    // Right, UP
+    [TD_PDHM]  = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_HOME),  // Page Down, HOME
+    [TD_PUED]  = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_END),   // Page Up  , END
 };
 
 // clang-format off
@@ -55,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   '    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |  Alt |  INS |  GUI |                                       | Left | Down |  Up  |Right |      |
+ *   |      |      |      |INS/AT|  GUI |                                       | LT/UP| RT/UP|      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        |      |      |       |      |        |
@@ -67,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_TRNS,         KC_TRNS,     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_GESC,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_TRNS,              KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LCTL_T(KC_TAB),  KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_TRNS,              KC_TRNS,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_TRNS,         KC_TRNS,     KC_LALT,       KC_INS,  KC_LGUI,                                         TD(TD_LD),   TD(TD_RU),KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,         KC_TRNS,     KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_GESC,         KC_Q,        KC_W,    KC_E,           KC_R,    KC_T,    KC_TRNS,              KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  LCTL_T(KC_TAB),  KC_A,        KC_S,    KC_D,           KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,         KC_Z,        KC_X,    KC_C,           KC_V,    KC_B,    KC_TRNS,              KC_TRNS,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+  KC_TRNS,         KC_TRNS,     KC_TRNS, LALT_T(KC_INS), KC_LGUI,                                         TD(TD_LD),   TD(TD_RU),KC_TRNS, KC_TRNS, KC_TRNS,
                                                                  KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS,
                                                                           KC_TRNS,                KC_TRNS,
                                               LT(_FUNC, KC_SPC), KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, LT(_NUMBER, KC_SPC)
@@ -425,7 +426,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(_FUNC, KC_SPC):
         case LT(_NUMBER, KC_SPC):
-            return TAPPING_TERM + 100;
+            return TAPPING_TERM + 50;
         default:
             return TAPPING_TERM;
     }
