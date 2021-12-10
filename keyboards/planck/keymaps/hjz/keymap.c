53ca5d3f14354422988ea1065e97f19f9dd9e09c
James Zhao <jamzhao@gmail.com>
2021-12-09 23:50:36 -0500

    Add rotory encoder support for planck ez hjz

--- keyboards/planck/keymaps/hjz/keymap.c
+++ keyboards/planck/keymaps/hjz/keymap.c
@@ -299,3 +299,14 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     }
 }
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    }
+    return false;
+}
