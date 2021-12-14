9bd3fa396ac2e2aba9879b42ef1f5773dceef851
James Zhao <jamzhao@gmail.com>
2021-12-14 13:34:48 -0500

    Add lily58 layer display

--- keyboards/lily58/keymaps/hjz/keymap.c
+++ keyboards/lily58/keymaps/hjz/keymap.c
@@ -169,22 +169,14 @@ void        set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
 const char *read_keylogs(void);
 
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         // If you want to change the display of OLED, you need to change here
         oled_write_ln(read_layer_state(), false);
-        oled_write_ln(read_keylog(), false);
-        oled_write_ln(read_keylogs(), false);
         // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
         // oled_write_ln(read_host_led_state(), false);
         // oled_write_ln(read_timelog(), false);
     } else {
-        oled_write(read_logo(), false);
     }
     return true;
 }
@@ -263,12 +255,6 @@ bool process_tmux_record(uint16_t keycode, keyrecord_t *record) {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-#ifdef OLED_ENABLE
-        set_keylog(keycode, record);
-#endif
-        // set_timelog();
-    }
     if ((keycode >= TMUX_Q) && (keycode <= TMUX_PLUS)) {
         return process_tmux_record(keycode, record);
     }
--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -2,32 +2,27 @@
 #include <stdio.h>
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_FUNC (1 << 1)
+#define L_SHIFT (1 << 2)
+#define L_TMUX (1 << 3)
+#define L_SYSTM (1 << 4)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
-    break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
-  }
+    if (layer_state & L_SYSTM) {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SYSTEM");
+    } else if (layer_state & L_TMUX) {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: TMUX");
+    } else if (layer_state & L_SHIFT) {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SHIFT");
+    } else if (layer_state & L_FUNC) {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNC");
+    } else if (layer_state == L_BASE) {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
+    } else {
+        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+    }
 
-  return layer_state_str;
+    return layer_state_str;
 }
