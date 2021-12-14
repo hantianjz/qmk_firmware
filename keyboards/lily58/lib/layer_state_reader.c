83cac217e8ef64178979c31b2268f2f953108f59
James Zhao <jamzhao@gmail.com>
2021-12-14 13:34:48 -0500

    Add lily58 layer display

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -2,32 +2,33 @@
 #include <stdio.h>
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_FUNC (1 << 1)
+#define L_SHIFT (1 << 2)
+#define L_TMUX (1 << 3)
+#define L_SYSTM (1 << 4)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
-    break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
-  }
+    switch (layer_state) {
+        case L_SYSTM:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SYSTEM");
+            break;
+        case L_TMUX:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: TMUX");
+            break;
+        case L_SHIFT:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SHIFT");
+            break;
+        case L_FUNC:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNC");
+            break;
+        case L_BASE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
+            break;
+        default:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+    }
 
-  return layer_state_str;
+    return layer_state_str;
 }
