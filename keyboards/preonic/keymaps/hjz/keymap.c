2e12374cc03b9b9eb6b6601322f1eb6d87e00c3a
James Zhao <hantian@google.com>
2022-05-19 10:39:20 -0400

    migrate key map to preonic

--- keyboards/preonic/keymaps/hjz/keymap.c
+++ keyboards/preonic/keymaps/hjz/keymap.c
@@ -35,15 +35,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  '   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | CTL  | Alt  | INS  | GUI  |_FUNC |    Space    |_SHIFT| Left | Down |  Up  |Right |
+     * | CTL  | Alt  | INS  | GUI  |_FUNC |  Spc |  Spc |_SHIFT| Left | Down |  Up  |Right |
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_ortho_5x12(                                                                                  // Start
         KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,                               // Row1
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,                                 // Row2
         LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,                      // Row3
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,                       // Row4
-        KC_LCTL, KC_LALT, KC_INS, KC_LGUI, MO(_FUNC), KC_SPC, KC_SPC, MO(_SHIFT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  // Row5
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,                       // Row4
+        KC_LCTL, KC_LALT, KC_INS, KC_LGUI, MO(_FUNC), LT(_FUNC, KC_SPC), LT(_SHIFT, KC_SPC), MO(_SHIFT), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  // Row5
+        ),
+
+    /* Function
+     * ,-----------------------------------------------------------------------------------.
+     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F0  |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |  UP  |      |      |      |      |      |      |      |      |      |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * | CTL  | LEFT | DOWN | RGHT |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |_TMUX | HOME | PGDN | PGUP |  END |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_FUNC] = LAYOUT_ortho_5x12(                                                                                     // Start
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,                     // Row1
+        KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    // Row2
+        KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,    // Row3
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  // Row4
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_TMUX), MO(_TMUX), KC_HOME, KC_PGDN, KC_PGUP, KC_END  // Row5
         ),
 
     /* Shift
@@ -64,28 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    // Row2
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, TMUX_COLON,  // Row3
         KC_TRNS, TMUX_Z, TMUX_X, TMUX_C, TMUX_V, KC_TRNS, TMUX_N, TMUX_M, TMUX_COMMA, KC_TRNS, KC_TRNS, KC_BSLS,       // Row4
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_TMUX), KC_TRNS, MO(_SYSTM), KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END      // Row5
-        ),
-
-    /* Function
-     * ,-----------------------------------------------------------------------------------.
-     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F0  |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |  UP  |      |      |      |      |      |      |      |      |      |
-     * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * | CTL  | LEFT | DOWN | RGHT |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
-     * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |             |      |_TMUX | HOME | PGDN | PGUP |  END |
-     * `-----------------------------------------------------------------------------------'
-     */
-    [_FUNC] = LAYOUT_ortho_5x12(                                                                                     // Start
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS,                     // Row1
-        KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    // Row2
-        KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS,    // Row3
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  // Row4
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_TMUX), KC_HOME, KC_PGDN, KC_PGUP, KC_END  // Row5
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_TMUX), MO(_TMUX), MO(_SYSTM), KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END      // Row5
         ),
 
     /* TMUX
