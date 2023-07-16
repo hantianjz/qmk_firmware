a20095a9f8b4ba3c33269f78ad1036e59ac1fd5a
James Zhao <hjz@hackjumpzero.ca>
2023-07-15 21:55:18 -0400

    save stuff

--- keyboards/lily58/keymaps/hjz/keymap.c
+++ keyboards/lily58/keymaps/hjz/keymap.c
@@ -36,7 +36,7 @@ enum {
 };
 
 // Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_LD]   = ACTION_TAP_DANCE_DOUBLE(KC_DOWN, KC_LEFT),  // Down, Left
     [TD_RU]   = ACTION_TAP_DANCE_DOUBLE(KC_UP, KC_RGHT),    // Up, Right
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL,
   LCTL_T(KC_TAB),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, _______,MO(_SYSTM),KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-        KC_INS,    KC_LALT,KC_LGUI, LT(_FUNC, KC_SPC),                LT(_SHIFT, KC_SPC), KC_RSPC, TD(TD_LD), TD(TD_RU) \
+        KC_INS,    KC_LALT,KC_LGUI, LT(_FUNC, KC_SPC),                LT(_SHIFT, KC_SPC), SC_RSPC, TD(TD_LD), TD(TD_RU) \
 ),
 /* FUNCTION
  * ,-----------------------------------------.                    ,-----------------------------------------.
